9198eae9323d416f1ee50e0d7ae689ba14504f0c
Omer Keskin <omerkeskin980@gmail.com>
2025-05-17 20:26:51 +0300

    Added time bar, back button, background image, time.

--- js/items.js
+++ js/items.js
@@ -1,6 +1,6 @@
 import { gameState, canvas } from "./myJs.js";
 
-class GameItem {
+class Ball {
     constructor(x, y, vx, vy,w,h, type, image,bounceFactor) {
       this.x = x;
       this.y = y;
@@ -20,18 +20,43 @@ class GameItem {
         this.vy += this.gravity;
         this.x += this.vx;
         this.y += this.vy;
-        
-        if(gameState.gameTime % 100*gameState.gameSpeed === 0)this.vx *= 0.99;
+        }
+        if(gameState.gameTime % 20 * gameState.gameSpeed === 0) this.vx *= 0.99;
         this.angle += 0.04*this.vx;
-    
-      if (this.y + this.height > canvas.height) {
-        this.y = canvas.height - this.height;
-        this.vy *= -this.bounceFactor;
-      }
-      if (this.x < 0 || this.x + this.width > canvas.width){
-        this.vx *= -1;
-      }
+      
+        if (this.y + this.height > canvas.height) {
+          this.y = canvas.height - this.height;
+          this.vy *= -this.bounceFactor;
+        }
+        if (this.x < 0 || this.x + this.width > canvas.width) this.vx *= -1;
     }
+    draw(ctx) {
+        ctx.save();
+        ctx.translate(this.x + this.width / 2, this.y + this.height / 2);
+        ctx.rotate(this.angle);
+        ctx.drawImage(this.image, -this.width / 2, -this.height / 2, this.width, this.height);
+        ctx.restore();
+    }
+  }
+
+  class Chest {
+    constructor(x, y, vx, vy,w,h, type, image,bounceFactor) {
+      this.x = x;
+      this.y = y;
+      this.vx = vx; 
+      this.vy = vy; 
+      this.type = type;
+      this.image = image; 
+      this.width = w;
+      this.height = h;
+    }
+    update() {
+        if(gameState.gameTime % gameState.gameSpeed === 0) {
+        this.x += this.vx;
+        this.y += this.vy;
+        }
+    
+        if (this.x < 0 || this.x + this.width > canvas.width) this.vx *= -1;
     }
     draw(ctx) {
         ctx.save();
@@ -44,4 +69,4 @@ class GameItem {
 
 
 
-  export { GameItem};
+  export { Ball };
