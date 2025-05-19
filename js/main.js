7b456944497ed89be8e4dc8f7fca795086a7bf44
Omer Keskin <omerkeskin980@gmail.com>
2025-05-20 02:40:29 +0300

    formatting

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-$(document).ready(function() {
-    $("#searchBtn").click(function() {
-      alert("Search feature coming soon!");
-    });
+$(document).ready(function () {
+  $("#searchBtn").click(function () {
+    alert("Search feature coming soon!");
   });
+});
 
-$(document).ready(function() {
-    $(".socials .icon").click(function() {
-      alert("Social media coming soon!");
-    });
+$(document).ready(function () {
+  $(".socials .icon").click(function () {
+    alert("Social media coming soon!");
+  });
 });
